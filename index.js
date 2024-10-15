3248b6649f45e1b8039a2abf8dc34b8bb20bb5a0
CAMILLEG <cgyrd@outlook.fr>
2024-10-15 14:13:49 +0200

    corretion en cours

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import chalk from 'chalk';
 
 
 function nomcolleague() {
-    // Créer un tableau avec les noms et leurs couleurs
+  
     const colleagues = [
         { name: 'Camille', color: chalk.red },
         { name: 'Bob', color: chalk.green },
@@ -10,11 +10,11 @@ function nomcolleague() {
         { name: 'Charles', color: chalk.yellow }
     ];
 
-    // Afficher chaque nom dans sa couleur respective
+
     colleagues.forEach(colleague => {
         console.log(colleague.color(colleague.name));
     });
 }
 
-// Appeler la fonction pour afficher les noms
+
 nomcolleague();
